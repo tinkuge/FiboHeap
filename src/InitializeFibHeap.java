ab1258cf9382c45d3a9d03e92b6e7e60302a7483
tinkuge <dinakarge@gmail.com>
2018-11-12 13:46:52 -0500

    added rest of the methos in fibheap

--- src/InitializeFibHeap.java
+++ src/InitializeFibHeap.java
@@ -1,7 +1,13 @@
+import java.util.ArrayList;
+import java.util.HashMap;
 
 class InitializeFibHeap {
 	Node max = null;
 	int size; //check if size is used anywhere
+	ArrayList<Node> rootList = new ArrayList<Node>();
+	HashMap<Integer, Node> degreeMap = new HashMap<Integer, Node>();
+	
+	
 	public boolean isEmpty(){
 		if(max == null){
 			return true;
@@ -61,50 +67,185 @@ class InitializeFibHeap {
 		}
 	}
 	
-	void increaseKey(Node n, int inc){
-		n.val = n.val + inc;
-		
-		if((n.val > n.parent.val) && (n.parent != null))
-			//implement childcut
-		if(n.val > max.val)
-			max = n;
-	}
 	
 	Node removeMax(){
 		Node altmax = null;
 		Node newHeap;
-		if(max.next == max)
-			max = null;
 		
-		else{
-			max.prev.next = max.next;
-			max.next.prev = max.prev;
-			//set max to something else
-			altmax = max;
-			max = max.next;
-			altmax.prev = null;
-			altmax.next = null;
-		}
+		//regardless of the fact that max has siblings, patch siblings
+		max.prev.next = max.next;
+		max.next.prev = max.prev;
+		
+		altmax = max;
+		//set max to something else which won't necessarily be the max of that tree
+		max = max.next;
+		//sever connections between siblings
+		altmax.prev = null;
+		altmax.next = null;
+		
 		
 		//if prev max has children
 		
-		if(altmax.degree != 0){	//track degree carefully
+		if(altmax.degree > 0){	//track degree carefully
 			Node maxchild = altmax.child;
-			//sever connections between parents and child
+			//sever connections between parent and children
 			maxchild.parent = null;
 			altmax.child = null;
 			while((maxchild.next != maxchild) && (maxchild.next != null)){
 				maxchild = maxchild.next;
 				maxchild.parent = null;
 			}
-			//Heap doesn't point to true max
+			//meld sibling tree with children of prev max.
+			//max pointer won't be pointing to true max
 			newHeap = meld(max, maxchild);
+			//find new max and assign it to max variable
+			//max = findMax(newHeap);
+			consolidate(newHeap);
+			return altmax;
 		}
 		
+		else{
+			return altmax;
+		}
 		
+	}
+	
+	//better to consolidate first and then find max because less roots to traverse to find roots
+	Node consolidate(Node heap){
+		max = heap;
+		rootList.add(heap);
+		Node ini = heap.next;
+		Node current, newTree;
+		while(ini != heap){
+			rootList.add(ini);
+			ini = ini.next;
+		}
 		
+		for(Node n: rootList){
+			
+			//only consolidate if the node has no parents
+			if(n.parent == null)
+				intoHM(n);
+		}
+		return max;
+	}
+	
+	//How will you keep track of the heap?
+	
+	void intoHM(Node insNode){
+		Node current = degreeMap.get(insNode.degree);
 		
+		/*
+		//check if the node is already in the hashmap
+		if(insNode == current) //https://stackoverflow.com/questions/13387742/compare-two-objects-with-equals-and-operator
+			return;
+		*/
 		
+		if(current == null){
+			
+			//compare max
+			if(insNode.val >= max.val)
+				max = insNode;
+			
+			degreeMap.put(insNode.degree, insNode);
+			return;
+		}
+		
+		else{
+			//remove the existing heap from degree table
+			degreeMap.remove(current.degree);
+			if(current.val >= insNode.val){
+				
+				//compare max
+				if(current.val >= max.val)
+					max = current;
+				//current is bigger, ins is smaller
+				insNode.next.prev = insNode.prev;
+				insNode.prev.next = insNode.next;
+				insNode.next = insNode;
+				insNode.prev = insNode;
+				insNode.parent = current;
+				
+				//just in case
+				current.parent = null;
+				current.child = meld(current.child, insNode);
+				current.child.parent = current;
+				current.degree = current.degree+1;
+				insNode.childCut = false;
+				intoHM(current);
+			}
+			
+			else{
+				
+				//compare max
+				if(insNode.val >= max.val)
+					max = insNode;
+				
+				current.next.prev = current.prev;
+				current.prev.next = current.next;
+				current.next = current;
+				current.prev = current;
+				current.parent = insNode;
+				insNode.parent = null;
+				insNode.child = meld(insNode.child, current);
+				insNode.child.parent = insNode;
+				insNode.degree = insNode.degree + 1;
+				current.childCut = false;
+				intoHM(insNode);
+			}
+		}
+
+		
+	}
+	
+	void increaseKey(Node n, int inc){
+		n.val = n.val + inc;
+		
+		if((n.val > n.parent.val) && (n.parent != null))
+			//implement childcut
+			
+			
+		if(n.val > max.val)
+			max = n;
+	}
+	
+	void childCut(Node n){
+		n.childCut = false;
+		Node parent = n.parent;
+		parent.degree = parent.degree - 1;
+		//patch n's siblings
+		n.prev.next = n.next;
+		n.next.prev = n.prev;
+		
+		if(parent.child == n){
+			if(n.next != n){
+				if(n.next.parent == parent){
+					parent.child = n.next;
+				}
+				else{//check if prev is linked to parent;
+					if(n.prev.parent == parent)
+						parent.child = n.prev;
+					
+					else
+						parent.child = null;
+				}
+			}
+			
+			else
+				parent.child = null;
+		}
+		
+		n.prev = null;
+		n.next = null;
+		n.parent = null;
+		
+		max = meld(max, n);
+		
+		if(parent.childCut == true)
+			childCut(parent);
+		
+		else
+			parent.childCut = true;
 		
 	}
 	
--- src/KeywordCounter.java
+++ src/KeywordCounter.java
@@ -2,15 +2,20 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.PrintWriter;
+import java.io.UnsupportedEncodingException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Scanner;
 
 public class KeywordCounter {
 
-	public static void main(String[] args) throws FileNotFoundException {
+	public static void main(String[] args) throws FileNotFoundException, UnsupportedEncodingException {
 		ArrayList<String> inpline = new ArrayList<String>();
 		HashMap<String, Integer> keyfreq = new HashMap<String, Integer>();
+		Node n, remMax;
+		InitializeFibHeap ifh = new InitializeFibHeap();
+		ArrayList<Node> nMaxList = new ArrayList<Node>();
 		
 		String inFile = "keywords.txt";
 		String fileLine = null;
@@ -19,9 +24,11 @@ public class KeywordCounter {
 		while(sc.hasNextLine()){
 			String readline = sc.nextLine();
 			inpline.add(readline);
-			System.out.println(readline);
+			//System.out.println(readline);
 			
 		}
+		
+		PrintWriter pw = new PrintWriter("output_file.txt", "UTF-8");
 		
 		for(String str: inpline){
 			if(str.charAt(0) == '$'){
@@ -34,8 +41,17 @@ public class KeywordCounter {
 				//Perform remove max n times
 				if(Character.isDigit(str.charAt(0))){
 					int num = Integer.parseInt(str);
+					
+					n = ifh.peekMax();
+					
 					for(int i = 0; i < num; i++){
 						//Call remove max
+						remMax = ifh.removeMax();
+						nMaxList.add(remMax);
+					}
+					
+					if(nMaxList.size() >= 1){
+						
 					}
 				}
 			}
