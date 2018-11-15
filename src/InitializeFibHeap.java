ce51bcb09ff28aa9e86db5490d775e0447979e45
tinkuge <dinakarge@gmail.com>
2018-11-15 16:22:10 -0500

    added comments

--- src/InitializeFibHeap.java
+++ src/InitializeFibHeap.java
@@ -3,11 +3,11 @@ import java.util.HashMap;
 
 class InitializeFibHeap {
 	Node max = null;
-	int size; //check if size is used anywhere
-	ArrayList<Node> rootList = new ArrayList<Node>();
-	HashMap<Integer, Node> degreeMap = new HashMap<Integer, Node>();
+	int size; //size of the Fibonacci heap
+	ArrayList<Node> rootList = new ArrayList<Node>();	//Rootlist to be used after remove max
+	HashMap<Integer, Node> degreeMap = new HashMap<Integer, Node>();	//degreetable
 	
-	
+	//Check if the fibonacci heap is empty
 	public boolean isEmpty(){
 		if(max == null){
 			return true;
@@ -17,23 +17,32 @@ class InitializeFibHeap {
 			return false;
 	}
 	
+	//return the max node
 	public Node peekMax(){
 		return max;
 	}
 	
+	
+	//Take the key and value and insert it into a tree and return the pointer to newly inserted node
 	Node insert(String key, int val){
+		
+		//create a node with gven key and value
 		Node n = new Node(key, val);
+		//set its degree to 0
 		n.degree = 0;
-		n.parent = null;
-		n.child = null;
-		n.childCut = false;
+		n.parent = null;	//set its parent to null
+		n.child = null;	//set itschild to null
+		n.childCut = false;	//set childcut to false
+		//check if max is null
 		if(max == null){
+			//set max to newly created node
 			max = n;
-			size++;
+			size = size+1;
 		}
 		else{
+			//meld node into the existing heap
 			max = meld(n, max);
-			size++;
+			size = size+1;
 		}
 		
 		return n;
@@ -45,15 +54,19 @@ class InitializeFibHeap {
 		
 		Node temp;
 		
+		//if both nodes are null, return null
 		if((n == null) &&(m == null))
 			return null;
 		
+		//if the heap is null but the newly created node is not null, return heap with newly created node
 		else if((m != null) && (n == null))
 			return m;
 		
+		//if the newly created node is null, return the heap
 		else if((n != null) && (m == null))
 			return n;
 		
+		//Otherwise, patch the newly created node into the existing heap list
 		else{
 			m.prev.next = n;
 			n.prev.next = m;
@@ -61,15 +74,17 @@ class InitializeFibHeap {
 			m.prev = n.prev;
 			n.prev = temp;
 			
+			//if the newly created node has a value greater than current max, return the heap with max pointer to new node
 			if(m.val > n.val)
 				return m;
 			
+			//simply return the heap
 			else
 				return n;			
 		}
 	}
 	
-	
+	//return the removed max node
 	Node removeMax(){
 		Node altmax = null;
 		Node newHeap;
@@ -93,6 +108,8 @@ class InitializeFibHeap {
 			//sever connections between parent and children
 			maxchild.parent = null;
 			maxchild = maxchild.next;
+			//if the parent's child pointer points to node under consideration, shift the pointer to next node,
+			//provided it is also a child of the parent.
 			while(maxchild != altmax.child && maxchild.parent == altmax){
 				maxchild.parent = null;
 				maxchild = maxchild.next;
@@ -106,12 +123,15 @@ class InitializeFibHeap {
 			//max = findMax(newHeap);
 			consolidate(newHeap);
 			size = size -1;
+			//return the removed max node
 			return altmax;
 		}
 		
 		else{
+			//if the node has no children, simply reorganize the residual tree such that it points to a new max node
 			consolidate(max);
 			size = size -1;
+			//return the removed node
 			return altmax;
 		}
 		
@@ -120,6 +140,7 @@ class InitializeFibHeap {
 	//better to consolidate first and then find max because less roots to traverse to find roots
 	Node consolidate(Node heap){
 		//max = heap;
+		//Traverse through the top level nodes and add them to the rootList
 		rootList.add(heap);
 		Node ini = heap.next;
 		while(ini != heap){
@@ -127,28 +148,27 @@ class InitializeFibHeap {
 			ini = ini.next;
 		}
 		
+		//for each top level node, call pairwise combine
 		for(Node n: rootList){
 			
 			//only consolidate if the node has no parents
 			if(n.parent == null)
 				pairwiseCombine(n);
 		}
+		
+		//clear the rootlist and degreemap
 		rootList.clear();
 		degreeMap.clear();
 		return max;
 	}
 	
-	//How will you keep track of the heap?
 	
+	//pairwise combine nodes with same degree
 	void pairwiseCombine(Node insNode){
+		//get the node with degree that is equal to the node under consideration
 		Node current = degreeMap.get(insNode.degree);
 		
-		/*
-		//check if the node is already in the hashmap
-		if(insNode == current) //https://stackoverflow.com/questions/13387742/compare-two-objects-with-equals-and-operator
-			return;
-		*/
-		
+		//if a node does not exist in the hashmap, insert the current node into hashmap
 		if(current == null){
 			
 			//compare max
@@ -162,6 +182,8 @@ class InitializeFibHeap {
 		else{
 			//remove the existing heap from degree table
 			degreeMap.remove(current.degree);
+			
+			//if the existing heap max node is greater than the inserted node, make the inserted node the child of existing heap
 			if(current.val >= insNode.val){
 				
 				//compare max
@@ -174,31 +196,41 @@ class InitializeFibHeap {
 				insNode.prev = insNode;
 				insNode.parent = current;
 				
-				//just in case
+				//meld the newly inserted node with children of existing heap
 				current.parent = null;
 				current.child = meld(current.child, insNode);
+				//set parent node of the child
 				current.child.parent = current;
+				//increase degree
 				current.degree = current.degree+1;
+				//set childcut value of the child to false
 				insNode.childCut = false;
+				//recursively call pairwise combine to insert the new heap into degree table
 				pairwiseCombine(current);
 			}
 			
+			//otherwise, make the existing heap a child of the inserted node
 			else{
 				
 				//compare max
 				if(insNode.val >= max.val)
 					max = insNode;
 				
+				//make existing heap a child of inserted node
 				current.next.prev = current.prev;
 				current.prev.next = current.next;
 				current.next = current;
 				current.prev = current;
+				//set parent value of existing heap
 				current.parent = insNode;
 				insNode.parent = null;
+				//meld the existing heap with children of inserted node
 				insNode.child = meld(insNode.child, current);
 				insNode.child.parent = insNode;
+				//increase degree
 				insNode.degree = insNode.degree + 1;
 				current.childCut = false;
+				//recursively call pairwise combine to insert the new heap into degree table
 				pairwiseCombine(insNode);
 			}
 		}
@@ -206,24 +238,30 @@ class InitializeFibHeap {
 		
 	}
 	
+	//increase the value of the node
 	void increaseKey(Node n, int inc){
 		n.val = n.val + inc;
 		
 		//if the node has parents and the node value is greater than its parents
 		if((n.parent != null) && (n.val > n.parent.val))
-			childCut(n);
-			
+			childCut(n);//call child cut if the node value is greater than its parents
+		
+		//check the max value
 		if(n.val > max.val)
 			max = n;
 	}
 	
+	
+	//Call for cascading cut
 	void childCut(Node n){
 		n.childCut = false;
 		Node parent = n.parent;
 		
+		//if the node is top level, no need to child cut
 		if(parent == null)
 			return;
 		
+		//otherwise, decrease the degree
 		parent.degree = parent.degree - 1;
 		
 		
@@ -232,17 +270,20 @@ class InitializeFibHeap {
 		n.prev.next = n.next;
 		n.next.prev = n.prev;
 		
-		
+		//if the parent's child is the current node
 		if(parent.child == n){
+			//if the node has siblings
 			if(n.next != n){
+				//if the next node's parent is also the same
 				if(n.next.parent == parent){
+					//shift child pointer to sibling
 					parent.child = n.next;
 				}
 				else{//check if prev is linked to parent;
 					if(n.prev.parent == parent)
 						parent.child = n.prev;
 					
-					else
+					else//if there are no children, set child pointer to null
 						parent.child = null;
 				}
 			}
@@ -254,17 +295,17 @@ class InitializeFibHeap {
 		n.prev = null;
 		n.next = null;
 		n.parent = null;
-		
+		//meld the node with top level nodes
 		max = meld(max, n);
-		
+		//if the parent's childcut value is already true
 		if(parent.childCut == true)
-			childCut(parent);
+			childCut(parent);	//recursively call childcut
 		
 		else
-			parent.childCut = true;
+			parent.childCut = true;//else, set childcut value to true
 		
 	}
-	
+	//get size of the heap
 	int getSize(){
 		return size;
 	}
--- src/keywordcounter.java
+++ src/keywordcounter.java
@@ -15,29 +15,46 @@ public class keywordcounter {
 		InitializeFibHeap ifh = new InitializeFibHeap();
 		ArrayList<Node> nMaxList = new ArrayList<Node>();
 		
+		//set the filename from command line argument
 		String inFile = args[0];
 		Scanner sc = new Scanner(new File(inFile));
 		
+		//while the file has a line to read in
 		while(sc.hasNextLine()){
 			String readline = sc.nextLine();
+			//add the line to the arraylist
 			inpline.add(readline);
 			//System.out.println(readline);
 			
 		}
 		
+		//Specify the output file for the print writer to write in
 		PrintWriter pw = new PrintWriter("output_file.txt", "UTF-8");
 		
+		//iterate through the arraylist
 		for(String str: inpline){
+			
+			//if the line starts with $
 			if(str.charAt(0) == '$'){
+				
+				//split the line by space
 				String[] splitline = str.split(" ");
+				
+				//Assign the word after $ to keyword
 				String keyword = splitline[0].substring(1);
+				//assign the word after the space to keyval
 				int keyval = Integer.parseInt(splitline[1]);
+				
+				//if the hashmap already contains the keyword, call increase key
 				if(keyNode.containsKey(keyword)){
+					//get the node pointer from hashmap
 					n = keyNode.get(keyword);
+					//call increase key
 					ifh.increaseKey(n, keyval);
 				}
 				
 				else{
+					//if the keyword is not in hashmap, insert it into hashmap and the fibonacci heap
 					n = ifh.insert(keyword, keyval);
 					keyNode.put(keyword, n);
 				}
@@ -47,22 +64,33 @@ public class keywordcounter {
 			}
 			
 			else{
-				//Perform remove max n times
+				//Perform remove max n times, if the line is an integer
 				if(Character.isDigit(str.charAt(0))){
 					int num = Integer.parseInt(str);
+					
+					//throw exception if the integer is greater than the size of the heap
+					if(num > ifh.getSize())
+						throw new ArrayIndexOutOfBoundsException("ZGiven value exceeds the size of the heap");
+					
+					
+					//get the max node form the heap
 					n = ifh.peekMax();
 					
+					//call remove max num times
 					for(int i = 0; i < num; i++){
 						//Call remove max
 						remMax = ifh.removeMax();
+						//add it to a list to reinsert later
 						nMaxList.add(remMax);
 					}
 					
+					//if the list size is greater than zero, print the keywords to output file
 					if(nMaxList.size() >= 1){
 						String word = nMaxList.get(0).key;
 						pw.write(word);
 					}
 					
+					//print subsequent words to the output
 					for(int j = 1; j < nMaxList.size(); j++){
 						String word = nMaxList.get(j).key;
 						pw.write(","+ word);
@@ -70,8 +98,11 @@ public class keywordcounter {
 					
 					pw.write("\n");
 					
+					
+					//reinsert all the nodes that were removed
 					for(int k = 0; k < nMaxList.size(); k++){
 						reNode = nMaxList.get(k);
+						//update the hashmap with the new node pointer
 						Node updated = ifh.insert(reNode.key, reNode.val);
 						
 						//should you insert it into hashmap
@@ -83,6 +114,7 @@ public class keywordcounter {
 				}
 				
 				else{
+					//if the line is 'stop', terminate the program
 					if(str.toLowerCase().equals("stop")){
 						pw.flush();
 						pw.close();
